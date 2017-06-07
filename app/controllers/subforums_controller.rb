4b76a2378dddfc37304a3967c08a4895daca65fc
Nick Savickas <nick.savickas@gmail.com>
2017-06-06 22:41:35 -0400

    styling2

--- app/controllers/subforums_controller.rb
+++ app/controllers/subforums_controller.rb
@@ -2,22 +2,22 @@ class SubforumsController < ApplicationController
   before_action :logged_in_user,  except: [:index]
   before_action :admin_user,      only: [:new, :edit, :destroy, :create, :update]
   before_filter :get_forum,       only: [:new, :index]
-       
+
   def new
     @subforum = Subforum.new
   end
-  
+
   def show
     @subforum = Subforum.find(params[:id])
   end
-  
+
   def index
     @subforums = @forum.subforums.order(:subforumname).page params[:page]
   end
-  
+
   def create
     @subforum = Subforum.new(subforum_params)
-    
+
     respond_to do |format|
       if @subforum.save
         format.html { redirect_to @subforum, notice: 'Subforum was successfully created.' }
@@ -28,30 +28,29 @@ class SubforumsController < ApplicationController
       end
     end
   end
-  
+
   def edit
     @subforum = Subforum.find(params[:id])
   end
-  
+
   def update
     @subforum = Subforum.find(params[:id])
     if @subforum.update_attributes(subforum_params)
-      # Handle a successful update.
       flash[:success] = "Subforum updated"
       redirect_to @subforum
     else
       render 'edit'
     end
   end
-  
+
   def destroy
     Subforum.find(params[:id]).destroy
     flash[:success] = "Subforum deleted"
     redirect_to :back
   end
-  
+
   private
-  
+
     def get_forum
       @forum = Forum.find(params[:forum_id])
     end
